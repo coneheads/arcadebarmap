f7d526ed223620b58ceba0cc7963be54138c1c1d
Daniel García Guillén <Daniel Garca Guilln>
2017-05-22 16:12:43 -0400

    This is new version with a map

---
+++ js/map.js
@@ -0,0 +1,41 @@
+var map;
+var infowindow;
+
+function initMap() {
+  var pyrmont = {lat: 40.7128, lng: -74.0059}; 
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pyrmont,
+    zoom: 12
+  });
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: pyrmont,
+    radius: 15000,
+    keyword: ["arcade", "videogames"],
+    type: ['bar']
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
